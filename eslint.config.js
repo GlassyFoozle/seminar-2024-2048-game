8da474af8440f408866d12a429d5022225522a12
Brian Kim <brian2562@gmail.com>
2024-11-26 17:25:55 +0900

    prettier fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 import react from '@woohm402/eslint-config-react';
 
 export default [
-  { 
+  {
     ignores: [
       'eslint.config.js',
       'tailwind.config.js',
       'postcss.config.js',
-      '.yarn'
-    ]
+      '.yarn',
+    ],
   },
   ...react({
     tsconfigRootDir: import.meta.dirname,
